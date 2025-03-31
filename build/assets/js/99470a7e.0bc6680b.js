3ff18764e5e835acd00cca1914a1b25a32fb4187
He333am Okati <hesamokati@gmail.com>
2025-04-01 00:05:55 +0430

    Deployment

---
+++ build/assets/js/99470a7e.0bc6680b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkjsam_docs=self.webpackChunkjsam_docs||[]).push([[1987],{8459:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/JSAM/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ build/assets/js/acecf23e.41b1b0dd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkjsam_docs=self.webpackChunkjsam_docs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/JSAM/blog","blogTitle":"Blog","authorsListPath":"/JSAM/blog/authors"}')}}]);
